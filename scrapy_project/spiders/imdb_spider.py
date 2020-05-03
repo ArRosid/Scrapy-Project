15b2811510cb8fc9823d616e7a74775431155881
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-05-03 15:38:30 +0700

    Modify imdb scraper to scrape multi page

--- scrapy_project/spiders/imdb_spider.py
+++ scrapy_project/spiders/imdb_spider.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from ..items import ImdbItem
+from time import sleep
+from random import randint
 
 class ImdbSpiderSpider(scrapy.Spider):
     name = 'imdb_spider'
@@ -14,9 +16,16 @@ class ImdbSpiderSpider(scrapy.Spider):
         for movie in all_movies:
             items['title'] = movie.xpath('.//h3/a/text()').extract_first()
             items['duration'] = movie.xpath('.//p[@class="text-muted "]/span[@class="runtime"]/text()').extract_first()
-            items['genre'] = movie.xpath('.//p[@class="text-muted "]/span[@class="genre"]/text()').extract_first()
+            items['genre'] = movie.xpath('normalize-space(.//p[@class="text-muted "]/span[@class="genre"]/text())').extract_first()
             items['imdb_rating'] = movie.xpath('.//div[@class="inline-block ratings-imdb-rating"]/strong/text()').extract_first()
-            items['metascore_rating'] = movie.xpath('.//div[@class="inline-block ratings-metascore"]/span/text()').extract_first()
+            items['metascore_rating'] = movie.xpath('normalize-space(.//div[@class="inline-block ratings-metascore"]/span/text())').extract_first()
             items['votes'] = movie.xpath('.//span[@name="nv"]/text()').extract_first()
 
             yield items
+
+        sleep(randint(2, 5))
+
+        next_page = response.xpath('//div[@class="desc"]/a/@href').extract_first()
+
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page))
