69dd3f42cf452f47b268beedea46592000c9a4b0
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-05-03 15:45:37 +0700

    not using item from imdb scraper

--- scrapy_project/spiders/imdb_spider.py
+++ scrapy_project/spiders/imdb_spider.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from ..items import ImdbItem
 from time import sleep
 from random import randint
 
@@ -10,18 +9,23 @@ class ImdbSpiderSpider(scrapy.Spider):
     start_urls = ['https://www.imdb.com/search/title/?release_date=2019-01-01,&sort=num_votes,desc']
 
     def parse(self, response):
-        items = ImdbItem()
-
         all_movies = response.xpath('//div[@class="lister-item mode-advanced"]')
         for movie in all_movies:
-            items['title'] = movie.xpath('.//h3/a/text()').extract_first()
-            items['duration'] = movie.xpath('.//p[@class="text-muted "]/span[@class="runtime"]/text()').extract_first()
-            items['genre'] = movie.xpath('normalize-space(.//p[@class="text-muted "]/span[@class="genre"]/text())').extract_first()
-            items['imdb_rating'] = movie.xpath('.//div[@class="inline-block ratings-imdb-rating"]/strong/text()').extract_first()
-            items['metascore_rating'] = movie.xpath('normalize-space(.//div[@class="inline-block ratings-metascore"]/span/text())').extract_first()
-            items['votes'] = movie.xpath('.//span[@name="nv"]/text()').extract_first()
+            title = movie.xpath('normalize-space(.//h3/a/text())').extract_first()
+            duration = movie.xpath('.//p[@class="text-muted "]/span[@class="runtime"]/text()').extract_first()
+            genre = movie.xpath('normalize-space(.//p[@class="text-muted "]/span[@class="genre"]/text())').extract_first()
+            imdb_rating = movie.xpath('.//div[@class="inline-block ratings-imdb-rating"]/strong/text()').extract_first()
+            metascore_rating = movie.xpath('normalize-space(.//div[@class="inline-block ratings-metascore"]/span/text())').extract_first()
+            votes = movie.xpath('.//span[@name="nv"]/text()').extract_first()
 
-            yield items
+            yield {
+                'title': title,
+                'duration': duration,
+                'genre': genre,
+                'imdb_rating': imdb_rating,
+                'metascore_rating': metascore_rating,
+                'votes': votes
+            }
 
         sleep(randint(2, 5))
 
